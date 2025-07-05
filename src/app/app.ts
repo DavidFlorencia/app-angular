1e1be35460554abdc383d34215e58637bcaf39d8
it-dflorenciac <dflorencia.developer@gmail.com>
2025-07-05 14:00:56 -0600

    primer_componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NuevoComponente } from "./nuevo-componente/nuevo-componente";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NuevoComponente],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'app-angular';
+  protected title = 'Hola mundo desde Angular!';
 }
