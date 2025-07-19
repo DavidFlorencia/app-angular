dc1ed521e011d80ed9f437971bb2e15f2d57c45f
it-dflorenciac <dflorencia.developer@gmail.com>
2025-07-19 12:34:17 -0600

    final_primer_modulo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponente } from "./nuevo-componente/nuevo-componente";
+import { ComponentInline } from "./component-inline/component-inline";
+import { Interpolacion } from "./interpolacion/interpolacion";
+import { Padre } from "./padre/padre";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponente],
+  imports: [RouterOutlet, NuevoComponente, ComponentInline, Interpolacion, Padre],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'Hola mundo desde Angular!';
+  protected title = 'Universidad Angular';
 }
