2e49c613f49d6d20ca099e8f71624c8b38c2e8cd
it-dflorenciac <dflorencia.developer@gmail.com>
2025-07-26 15:04:36 -0600

    directivas

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,13 +8,16 @@ import { Hijo } from "./hijo/hijo";
 import { MostrarMensaje } from "./mostrar-mensaje/mostrar-mensaje";
 import { Replicador } from "./replicador/replicador";
 import { Saludar } from "./saludar/saludar";
+import { ComponenteIf } from "./componente-if/componente-if";
+import { AgregarTarea } from "./agregar-tarea/agregar-tarea";
+import { ComponenteFor } from "./componente-for/componente-for";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponente, ComponentInline, Interpolacion, Padre, Hijo, MostrarMensaje, Replicador, Saludar],
+  imports: [RouterOutlet, NuevoComponente, ComponentInline, Interpolacion, Padre, Hijo, MostrarMensaje, Replicador, Saludar, ComponenteIf, AgregarTarea, ComponenteFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'Two Way Binding en Angular';
+  protected title = 'Directiva @for en Angular';
 }
